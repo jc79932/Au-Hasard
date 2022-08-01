ae30549ffd32517149ebd0b0bf871fcd016cda87
jc79932 <jc79932@gmail.com>
2022-08-01 04:46:57 -0500

    Completed most JS code, missing pw length

--- script.js
+++ script.js
@@ -2,18 +2,56 @@
 //Slider code
 var slider = document.getElementById("slider-value");
 var output = document.getElementById("slider-value-output");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
+output.innerHTML = slider.value; 
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
 
-
-
 // Assignment code here
+let Lc = document.getElementById("lc"); 
+let Uc = document.getElementById("uc");
+let Nc = document.getElementById("nc");
+let Sc = document.getElementById("sc");
+const combUc = 26 - 1;
+const combLc = 52 - 1;
+const combNc = 62 - 1;
+const combSc = 92 - 1;
+let SVal = document.getElementById("slider-value-output");
 
+const combinations = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
+let temp = 1;
+let tempPassword = "";
+let testSVal = 10;
+console.log(combinations.charAt(Math.floor(Math.random() * 92) + 1));
+console.log(Lc.checked, Uc, Nc, Sc);
+
+function generatePassword(){
+  tempPassword = "";
+  for (let i=0; i<10;i++){
+    temp = Math.floor(Math.random() * 92) + 1;
+    console.log(temp);
+    if ((Uc.checked==true) && (temp<=combUc)){
+      tempPassword = tempPassword + combinations.charAt(temp);
+      
+    } else if ((Lc.checked==true) && (temp<=combLc) && (temp>combUc)){
+      tempPassword = tempPassword + combinations.charAt(temp);
+      
+    } else if ((Nc.checked==true) && (temp<=combNc) && (temp>combLc)){
+      tempPassword = tempPassword + combinations.charAt(temp);
+      
+    } else if ((Sc.checked==true) && (temp<=combSc) && (temp>combNc)){
+      tempPassword = tempPassword + combinations.charAt(temp);
+      
+    }  else{
+        i--;
+        
+      }
+      console.log(tempPassword);
+    }
+  
+  return tempPassword;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
